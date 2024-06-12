c82726b73cd9dd65224c056050d91d86bc7b4e3a
Alejandro <74669960+alma8026@users.noreply.github.com>
2024-06-12 23:21:37 +0200

    front added

--- Hackathon/App1/static/script.js
+++ Hackathon/App1/static/script.js
@@ -1,1 +1,30 @@
-console.log("adasdads");
+let currentSlide = 0;
+const slides = document.querySelectorAll('.carousel img');
+const indicators = document.querySelectorAll('.carousel-indicators button');
+let slideInterval;
+
+function showSlide(index) {
+  slides[currentSlide].classList.remove('active');
+  indicators[currentSlide].classList.remove('active');
+  currentSlide = index;
+  slides[currentSlide].classList.add('active');
+  indicators[currentSlide].classList.add('active');
+  resetSlideInterval();
+}
+
+function nextSlide() {
+  let nextSlide = (currentSlide + 1) % slides.length;
+  showSlide(nextSlide);
+}
+
+function prevSlide() {
+  let prevSlide = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide(prevSlide);
+}
+
+function resetSlideInterval() {
+  clearInterval(slideInterval);
+  slideInterval = setInterval(nextSlide, 3000);
+}
+
+slideInterval = setInterval(nextSlide, 3000);
